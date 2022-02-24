9b599e2a217a3b5746560b7bcfef1c2893bc438e
Merwyn Patrick <merwynpatrick@Merwyns-Mac-mini.local>
2022-02-24 18:02:59 -0500

    update props using useState hook

--- src/components/Application.js
+++ src/components/Application.js
@@ -36,8 +36,8 @@ export default function Application(props) {
         <nav className="sidebar__menu">
           <DayList
             days={days}
-            day={"Monday"}
-            setDay={day => console.log(day)}
+            day={day}
+            setDay={setDay}
           />
         </nav>
         <img
