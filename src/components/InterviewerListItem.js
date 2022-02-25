78e415e1776d425fe443a6a7df5891a9f3584c16
Merwyn Patrick <merwynpatrick@Merwyns-Mac-mini.local>
2022-02-25 10:24:45 -0500

    add InterviewerListItem component to stories

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import "components/InterviewerListItem.scss";
+import classNames from 'classnames';
+
+export default function InterviewerListItem(props) {
+  const interviewClasses = classNames(
+    "interviewers__item",
+    {"interviewers__item--selected": props.selected}
+  );
+  return (
+    <li className={interviewClasses}>
+      <img
+        className="interviewers__item-image"
+        src="https://i.imgur.com/LpaY82x.png"
+        alt="Sylvia Palmer"
+      />
+      Sylvia Palmer
+    </li>
+  );
+};
