6dc168fb4270b328376dd47a1b3aa9cb22a13cff
Merwyn Patrick <merwynpatrick@Merwyns-Mac-mini.local>
2022-02-27 08:25:28 -0500

    add Show component to stories

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import "components/Appointment/styles.scss";
 import Header from './Header';
 import Empty from './Empty';
+import Show from './Show';
 
 export default function Appointment(props) {
   return (
--- stories/index.js
+++ stories/index.js
@@ -13,6 +13,7 @@ import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment";
 import Header from "components/Appointment/Header";
 import Empty from "components/Appointment/Empty";
+import Show from "components/Appointment/Show";
 
 storiesOf("Button", module)
   .addParameters({
@@ -145,4 +146,17 @@ storiesOf("Appointment", module)
   .add("Appointment", () => <Appointment />)
   .add("Appointment with time", () => <Appointment time={"12pm"} />)
   .add("Header", () => <Header time={"12pm"} />)
-  .add("Empty", () => <Empty onClick={action("onAdd")}/>)
+  .add("Empty", () => <Empty onClick={action("onAdd")} />)
+
+storiesOf("Show", module)
+  .addParameters({
+    backgrounds: [{ name: "white", value: "#fff", default: true }]
+  })
+  .add("Show", () => (
+    <Show 
+    onEdit={action("onEdit")}
+    onDelete={action("onDelete")}
+    student={"Lydia Miller-Jones"}
+    interviewer={interviewer}
+    />
+  ))
