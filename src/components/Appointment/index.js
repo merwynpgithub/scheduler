4395974a400b1c3d102fac5e0e258fd42b051d19
Merwyn Patrick <merwynpatrick@Merwyns-Mac-mini.local>
2022-02-27 09:05:59 -0500

    add Error component to Appointment stories

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -5,6 +5,7 @@ import Empty from './Empty';
 import Show from './Show';
 import Confirm from './Confirm';
 import Status from './Status';
+import Error from './Error';
 
 export default function Appointment(props) {
   return (
