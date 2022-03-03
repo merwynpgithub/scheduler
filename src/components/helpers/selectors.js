fe73a83b0a78053cabee4e83e06122341d797d70
Merwyn Patrick <merwynpatrick@Merwyns-Mac-mini.local>
2022-03-03 11:31:53 -0500

    add helpers and test

---
+++ src/components/helpers/selectors.js
@@ -0,0 +1,13 @@
+export function getAppointmentsForDay(state, day) {
+  let result = [];
+  state.days.forEach(item => {
+    if (item.name === day) {
+      const appointmentList = item.appointments;
+      appointmentList.forEach(i => {
+        result.push(state.appointments[i]);
+      })
+      return result;
+    };
+  })
+  return result;
+};
