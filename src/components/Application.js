ce0c91c3211f4ee1637eea74ad8fda20c1e71fa5
Merwyn Patrick <merwynpatrick@Merwyns-Mac-mini.local>
2022-03-03 14:17:51 -0500

    modify state in Application component

--- src/components/Application.js
+++ src/components/Application.js
@@ -45,13 +45,19 @@ const appointments = {
 };
 
 export default function Application(props) {
-  const [days, setDays] = useState([]);
-  const [day, setDay] = useState("Monday");
+
+  const [state, setState] = useState({
+    day: "Monday",
+    days: []
+  });
+
+  const setDay = day => setState(Object.assign({}, state, { day }));
+  const setDays = days => setState(Object.assign({}, state, { days }));
 
   useEffect(() => {
     axios.get("/api/days")
     .then(res => {
-      setDays([...res.data]);
+      setDays(res.data);
     });
   }, []);
 
@@ -75,8 +81,8 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList
-            days={days}
-            value={day}
+            days={state.days}
+            value={state.day}
             onChange={setDay}
           />
         </nav>
