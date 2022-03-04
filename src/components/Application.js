561cf7acf28f03ad9fb8b502c4a58cc3398155c2
Merwyn Patrick <merwynpatrick@Merwyns-Mac-mini.local>
2022-03-04 09:22:12 -0500

    render interviewer data in application component

--- src/components/Application.js
+++ src/components/Application.js
@@ -4,7 +4,7 @@ import DayList from "./DayList";
 import "components/Application.scss";
 import axios from "axios";
 
-import { getAppointmentsForDay } from "helpers/selectors";
+import { getAppointmentsForDay, getInterview } from "helpers/selectors";
 
 export default function Application(props) {
 
@@ -32,10 +32,12 @@ export default function Application(props) {
   }, []);
 
   const appointmentList = dailyAppointments.map(appointment => {
+    const interview = getInterview(state, appointment.interview);
     return (
       <Appointment 
         key={appointment.id}
         {...appointment}
+        interview={interview}
       />
     );
   });
