7c475a16aff4e8cb9c4af037a4ac67af35a645ed
Merwyn Patrick <merwynpatrick@Merwyns-Mac-mini.local>
2022-03-05 09:32:44 -0500

    add back function in useVisualMode

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,9 +2,29 @@ import React, { useState } from 'react';
 
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
 
   const transition = (next) => {
+    setHistory(prev => {
+      const prevChange = prev;
+      prevChange.push(next);
+      setHistory(prevChange);
+    });
     setMode(next);
   }
-  return { mode, transition };
+
+  const back = () => {
+    setHistory(prev => {
+      const newChange = prev;
+      newChange.pop();
+      setHistory(newChange);
+    })
+    if (history.length >= 2) {
+      setMode(history[history.length - 2]);
+    } else {
+      setMode(history[0]);
+    }
+  };
+
+  return { mode, transition, back };
 };
